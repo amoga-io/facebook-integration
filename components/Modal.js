c2bb727ead5ef087a924f76ab00fb8e1d5d1570b
sharath-amoga <sharath.kumar@amoga.io>
2024-01-12 14:54:57 +0530

    modal height changed

--- components/Modal.js
+++ components/Modal.js
@@ -33,7 +33,7 @@ export default function Example({ open, setOpen }) {
 
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+            <Dialog as="div" className="relative z-100 mt-10" initialFocus={cancelButtonRef} onClose={setOpen}>
                 <Transition.Child
                     as={Fragment}
                     enter="ease-out duration-300"
@@ -46,7 +46,7 @@ export default function Example({ open, setOpen }) {
                     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
                 </Transition.Child>
 
-                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="fixed inset-0 z-10 w-screen overflow-y-auto ">
                     <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                         <Transition.Child
                             as={Fragment}
@@ -58,13 +58,13 @@ export default function Example({ open, setOpen }) {
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
                             <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                <div className="bg-white px-2 pb-2 pt-2 sm:p-6 sm:pb-4">
                                     <iframe
                                         src={"https://canvas2.amoga.app/applications/4fb3b93a-c322-4565-94ec-5d3dd1076e37?height=500"}
                                         title="iframe"
                                         style={{
                                             width: "100%",
-                                            height: "60vh",
+                                            height: "78vh",
                                             border: "none",
                                         }}></iframe>
                                 </div>
